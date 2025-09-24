18b70f0f816f03ced775bbbb22b1b0561a9b8310
sharebravery <sharebravery@gmail.com>
2025-09-24 15:56:27 +0800

    Refactor code structure for improved readability and maintainability

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -10,13 +10,10 @@ import { version } from "../../package.json";
 
 export default hopeTheme({
   // hostname: "https://sharebravery.github.io",
-
   author: {
     name: "sharebravery",
     url: "https://github.com/sharebravery",
   },
-
-  iconAssets: "fontawesome-with-brands",
 
   logo: "/logo.svg",
 
@@ -24,15 +21,8 @@ export default hopeTheme({
 
   docsDir: "docs",
 
-  // themeColor: {
-  //   blue: "#2196f3",
-  //   red: "#f26d6d",
-  //   green: "#3eaf7c",
-  //   orange: "#fb9b5f",
-  // },
-
   // navbar
-  navbar: navbar,
+  navbar,
 
   navbarLayout: {
     start: ["Brand"],
@@ -43,7 +33,7 @@ export default hopeTheme({
   // sidebar
   sidebar: "structure",
 
-  footer: `Apache Licensed | Copyright © 2023-present sharebravery v${version}`,
+  footer: `Apache Licensed | Copyright © 2025-present sharebravery v${version}`,
 
   displayFooter: true,
 
@@ -63,6 +53,77 @@ export default hopeTheme({
   //     "/guide/encrypt.html": ["1234"],
   //   },
   // },
+
+  markdown: {
+    align: true,
+    attrs: true,
+    codeTabs: true,
+    component: true,
+    demo: true,
+    figure: true,
+    imgLazyload: true,
+    imgSize: true,
+    include: true,
+    mark: true,
+    stylize: [
+      {
+        matcher: "Recommended",
+        replacer: ({ tag }) => {
+          if (tag === "em")
+            return {
+              tag: "Badge",
+              attrs: { type: "tip" },
+              content: "Recommended",
+            };
+        },
+      },
+    ],
+    sub: true,
+    sup: true,
+    tabs: true,
+    vPre: true,
+
+    // install chart.js before enabling it
+    // chart: true,
+
+    // insert component easily
+
+    // install echarts before enabling it
+    // echarts: true,
+
+    // install flowchart.ts before enabling it
+    // flowchart: true,
+
+    // gfm requires mathjax-full to provide tex support
+    // gfm: true,
+
+    // install katex before enabling it
+    math: {
+      // 启用前安装 katex
+      type: "katex",
+    },
+
+    // install mathjax-full before enabling it
+    // mathjax: true,
+
+    // install mermaid before enabling it
+    mermaid: true,
+
+    // playground: {
+    //   presets: ["ts", "vue"],
+    // },
+
+    // install reveal.js before enabling it
+    // revealJs: {
+    //   plugins: ["highlight", "math", "search", "notes", "zoom"],
+    // },
+
+    // install @vue/repl before enabling it
+    // vuePlayground: true,
+
+    // install sandpack-vue3 before enabling it
+    // sandpack: true,
+  },
 
   plugins: {
     blog: {
@@ -92,72 +153,6 @@ export default hopeTheme({
       // serverURL: "https://vuepress-theme-hope-comment.vercel.app",
     },
 
-    mdEnhance: {
-      align: true,
-      attrs: true,
-      codetabs: true,
-      component: true,
-      demo: true,
-      figure: true,
-      imgLazyload: true,
-      imgSize: true,
-      include: true,
-      mark: true,
-      stylize: [
-        {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
-              return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
-              };
-          },
-        },
-      ],
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
 
-      // install chart.js before enabling it
-      // chart: true,
-
-      // insert component easily
-
-      // install echarts before enabling it
-      // echarts: true,
-
-      // install flowchart.ts before enabling it
-      // flowchart: true,
-
-      // gfm requires mathjax-full to provide tex support
-      // gfm: true,
-
-      // install katex before enabling it
-      katex: true,
-
-      // install mathjax-full before enabling it
-      // mathjax: true,
-
-      // install mermaid before enabling it
-      mermaid: true,
-
-      // playground: {
-      //   presets: ["ts", "vue"],
-      // },
-
-      // install reveal.js before enabling it
-      // revealJs: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
-
-      // install @vue/repl before enabling it
-      // vuePlayground: true,
-
-      // install sandpack-vue3 before enabling it
-      // sandpack: true,
-    },
   },
 });
